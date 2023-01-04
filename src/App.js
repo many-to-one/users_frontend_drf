5f9785167df3920b44b8323de449eb42293f8080
many-to-one <xllxlex@gmail.com>
2023-01-04 15:01:25 +0100

    working on privateroute

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Header from './components/Header';
+import PrivateRoute from './utils/PrivateRoute';
 
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <BrowserRouter>
       <Header/>
         <Routes>
-          <Route path='/' exact element={<Home/>} />
+          <PrivateRoute path='/' exact element={<Home/>} />
           <Route path='/login' exact element={<Login/>} />
         </Routes>
       </BrowserRouter>
